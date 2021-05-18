d67e4a60add8ae08b4011bd6b7dcbe4bb1734a4e
Engels Quintero <engelsquintero@gmail.com>
2021-05-18 17:19:14 -0400

    Added guild penalty option

--- vSRO-GameServer/AppManager.cpp
+++ vSRO-GameServer/AppManager.cpp
@@ -53,6 +53,7 @@ void AppManager::InitConfigFile()
 		ini.SetLongValue("Server", "PARTY_MOB_SPAWN_PROBABILITY", 50, "; % Probability for party mob spawns");
 		ini.SetLongValue("Job", "LEVEL_MAX", 7, "; Maximum level that can be reached on job suit");
 		ini.SetLongValue("Race", "CH_TOTAL_MASTERIES", 330, "; Masteries amount Chinese will obtain");
+		ini.SetLongValue("Guild", "LEAVING_PENALTY_TIME", 259200, "; Penalty time (seconds) to join again to guild group");
 		ini.SetLongValue("Guild", "MEMBERS_LIMIT_LEVEL1", 15, "; Guild members capacity at level 1");
 		ini.SetLongValue("Guild", "MEMBERS_LIMIT_LEVEL2", 20, "; Guild members capacity at level 2");
 		ini.SetLongValue("Guild", "MEMBERS_LIMIT_LEVEL3", 25, "; Guild members capacity at level 3");
@@ -63,7 +64,7 @@ void AppManager::InitConfigFile()
 		ini.SetLongValue("Guild", "UNION_LIMIT", 8, "; Union participants limit");
 		ini.SetLongValue("Guild", "UNION_CHAT_PARTICIPANTS", 12, "; Union chat participants allowed by guild");
 		ini.SetLongValue("Academy", "GRADUATE_BEGINNER_LEVEL", 40, "; Graduation level for the beginner members");
-		ini.SetLongValue("Academy", "DISBAND_PENALTY_TIME", 604800, "; Penalty time to create again the academy group");
+		ini.SetLongValue("Academy", "DISBAND_PENALTY_TIME", 604800, "; Penalty time (seconds) to create again the academy group");
 		ini.SetValue("Event","CTF_ITEM_REWARD","ITEM_ETC_E070919_TROPHY","; Item reward from Capture The Flag");
 		ini.SetLongValue("Event", "CTF_ITEM_REWARD_AMOUNT", 1, "; Amount to obtain per every kill");
 		ini.SetValue("Event","BA_ITEM_REWARD","ITEM_ETC_ARENA_COIN","; Item reward from Battle Arena");
@@ -234,6 +235,14 @@ void AppManager::InitPatchValues()
 	}
 
 	// Guild
+	if (ReadMemoryValue<uint32_t>(0x005C3F94 + 1, uintValue))
+	{
+		uint32_t newValue = ini.GetLongValue("Guild", "LEAVING_PENALTY_TIME", 259200);
+		printf(" - GUILD_LEAVING_PENALTY_TIME (%u) -> (%u)\r\n", uintValue, newValue);
+		WriteMemoryValue<uint32_t>(0x005C3F94 + 1, newValue);
+		WriteMemoryValue<uint32_t>(0x009DF194 + 1, newValue);
+		WriteMemoryValue<uint32_t>(0x005C8B3D + 1, newValue);
+	}
 	{
 		uint32_t addr = 0x00ADE8DC;
 		if (ReadMemoryValue<uint32_t>(addr, uintValue))
@@ -306,7 +315,7 @@ void AppManager::InitPatchValues()
 	if (ReadMemoryValue<uint32_t>(0x005DD36A + 1, uintValue))
 	{
 		uint32_t newValue = ini.GetLongValue("Academy", "DISBAND_PENALTY_TIME", 604800);
-		printf(" - ACADEMY_DISBAND_PENALTY_TIME (%d) -> (%d)\r\n", uintValue, newValue);
+		printf(" - ACADEMY_DISBAND_PENALTY_TIME (%u) -> (%u)\r\n", uintValue, newValue);
 		WriteMemoryValue<uint32_t>(0x005DD36A + 1, newValue);
 		WriteMemoryValue<uint32_t>(0x00651C7A + 2, newValue);
 	}
